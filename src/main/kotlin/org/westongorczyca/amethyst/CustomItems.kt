890013750db27f91f9e6bd508c9f49bcde195051
OB <obear@OBs-MacBook-Air.local>
2026-05-31 13:13:58 -0400

    e

--- src/main/kotlin/org/westongorczyca/amethyst/CustomItems.kt
+++ src/main/kotlin/org/westongorczyca/amethyst/CustomItems.kt
@@ -37,7 +37,7 @@ object CustomItems {
                 persistentDataContainer.set(templateShardKey, PersistentDataType.BOOLEAN, true)
                 
                 // NEW: Direct String Item Model Assignment
-                setItemModel(shardModelKey)
+                setCustomModelDataString(shardModelKey)
             }
         }
     }
@@ -49,7 +49,7 @@ object CustomItems {
                 persistentDataContainer.set(smithingTemplateKey, PersistentDataType.BOOLEAN, true)
                 
                 // NEW: Direct String Item Model Assignment
-                setItemModel(templateModelKey)
+                setCustomModelDataString(templateModelKey)
             }
         }
     }
@@ -88,7 +88,7 @@ object CustomItems {
                 persistentDataContainer.set(echoPickaxeKey, PersistentDataType.BOOLEAN, true)
                 
                 // NEW: Direct String Item Model Assignment
-                setItemModel(pickaxeModelKey)
+                setCustomModelDataString(pickaxeModelKey)
             }
         }
     }
